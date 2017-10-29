2ac61cf2047ead64eeb85da3e586208ce4a7b10b
gastongerber <gastongerber1@gmail.com>
2017-10-29 20:41:45 -0300

    Update TrabajoFinalTest.php

--- tests/TrabajoFinalTest.php
+++ tests/TrabajoFinalTest.php
@@ -4,7 +4,7 @@ namespace TpFinal;
 
 use PHPUnit\Framework\TestCase;
 
-class TrabajoFinalTest extends TestCase {
+class TarjetaTest extends TestCase {
      public function testTarjeta() {
         $tarjeta = new Tarjeta(41655478);
         $this->assertEquals($tarjeta->saldo(), 0);
