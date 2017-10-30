9c0b5249a35b167be416dc01e30739af5d14d3f3
gastongerber <gastongerber1@gmail.com>
2017-10-29 21:01:30 -0300

    Update Viaje.php

--- src/Viaje.php
+++ src/Viaje.php
@@ -1,1 +1,20 @@
+<?php
+namespace TpFinal;
 
+class Viaje {
+    
+    protected $transporte, $fecha, $fechaf;
+
+    public function __construct ($trans,$f) {
+        $this->transporte=$trans;
+        $this->fecha=$f;
+	$this->fechaf=$f->format('Y-m-d-H-i-s');
+    }
+    
+    public function darTransporte() {
+          return $this->transporte;
+    }
+    public function darFecha() {
+          return $this->fecha;
+    }
+}
