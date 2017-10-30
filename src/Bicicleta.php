7390d449391f22136e104bd39fce0a42a949320d
gastongerber <gastongerber1@gmail.com>
2017-10-29 21:02:52 -0300

    Update Bicicleta.php

--- src/Bicicleta.php
+++ src/Bicicleta.php
@@ -1,1 +1,11 @@
+<?php
 
+namespace TpFinal;
+
+class Bicicleta extends Transporte {
+	public $boleto = 14.55;
+	public $patente;
+	public function __construct( $patente ) {
+		$this->patente = $patente;
+	}
+}
