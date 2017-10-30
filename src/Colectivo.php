3df08fa9a9e640f2e3a59b2e82ae8d6715bdd83e
gastongerber <gastongerber1@gmail.com>
2017-10-29 21:02:13 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,1 +1,12 @@
+<?php
+namespace TpFinal;
+
+class Colectivo extends Transporte {
+	public $boleto = 9.7;
+	public $linea;
+	public function __construct( $linea ) {
+		$this->linea = $linea;
+	}
+}
+
 
