e1eea1260a474cbee3f5aaae5e33e40cfd94761b
joaquinortega1 <32199369+joaquinortega1@users.noreply.github.com>
2017-10-31 01:02:36 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,12 +1,13 @@
 <?php
 namespace TpFinal;
 
-class Colectivo extends Transporte {
-	public $boleto = 9.7;
-	public $linea;
-	public function __construct( $linea ) {
-		$this->linea = $linea;
-	}
+class Colectivo extends Transporte
+{
+    public $boleto = 9.7;
+    public $linea;
+    public function __construct($linea) {
+        $this->linea = $linea;
+    }
 }
 
 
