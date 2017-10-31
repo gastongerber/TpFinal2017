45ac485c5cbf2dc17f29df0885e422f6f605e0fa
gastongerber <gastongerber1@gmail.com>
2017-10-31 14:01:04 -0300

    Update TrabajoFinalTest.php

--- tests/TrabajoFinalTest.php
+++ tests/TrabajoFinalTest.php
@@ -30,5 +30,32 @@ class TarjetaTest extends TestCase {
           $colectivo_113 = new Colectivo("113");
           $tarjeta->pagar($colectivo_113);
           $this->assertEquals($tarjeta->saldo(), ($saldo_actual - ($colectivo_113->boleto /3) ) );
+          
+          $saldo_actual = $tarjeta->saldo();
+          $tarjeta->pagar($colectivo_113);
+          $this->assertEquals($tarjeta->saldo(), ($saldo_actual - $colectivo_113->boleto ) );
+          
+          $saldo_actual = $tarjeta->saldo();
+          $colectivo_115 = new Colectivo("115");
+          $tarjeta->pagar($colectivo_115);
+          $this->assertEquals($tarjeta->saldo(), ($saldo_actual - ($colectivo_115->boleto /3) ) );
+          
+          $saldo_actual = $tarjeta->saldo();
+          $tarjeta->pagar($colectivo_115);
+          $this->assertEquals($tarjeta->saldo(), ($saldo_actual - ($colectivo_115->boleto /3) ) );
+          
+          $saldo_actual = $tarjeta->saldo();
+          $bici1 = new Bicicleta("11111");
+          $tarjeta->pagar($bici1);
+          $this->assertEquals($tarjeta->saldo(), ($saldo_actual - $bici1->boleto) );
+          
+          $saldo_actual = $tarjeta->saldo();
+          $bici2 = new Bicicleta("22222");
+          $bici3 = new Bicicleta("33333");
+          $bici4 = new Bicicleta("44444");
+          $tarjeta->pagar($bici2);
+          $tarjeta->pagar($bici3);
+          $tarjeta->pagar($bici4);
+          $this->assertEquals($tarjeta->saldo(), $saldo_actual );
     }
 }
